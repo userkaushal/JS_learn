0ddfea92639901d570f859f3f9a49a283df7d05f
kaushal <99007202+userkaushal@users.noreply.github.com>
2024-01-18 18:02:11 +0000

    date

---
+++ 07_dates.js
@@ -0,0 +1,11 @@
+// Dates
+
+let myDate = new Date();
+console.log(myDate);
+console.log(typeof myDate);
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toISOString());
+console.log(myDate.toJSON());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toLocaleString());
