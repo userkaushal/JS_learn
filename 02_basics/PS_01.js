46cbb9f5357e8dfbf9e83f6f13c4b3de4d5f7db5
Kaushal Mahida <99007202+userkaushal@users.noreply.github.com>
2024-06-10 06:14:37 +0000

    practice set-01

---
+++ 02_basics/PS_01.js
@@ -0,0 +1,32 @@
+//Q1
+
+let a = "str";
+let b = 123;
+
+console.log(a+b);
+
+//Q2
+console.log(typeof (a+b));
+
+//Q3
+const obj = {
+    name: "krish",
+    student: true
+}
+// obj = 54; error
+
+//Q4
+
+obj["name"]= "yash";
+obj["friend"] = "kaushal"
+console.log(obj["name"]);
+console.log(obj);
+
+//Q5
+
+const dict = {
+    yakka: "hard work"
+}
+
+console.log(dict.yakka);
+console.log(dict["yakka"]);
