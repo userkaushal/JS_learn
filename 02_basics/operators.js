76ce91bc49563a9d0dbc039d2f3cb5cf406f9f48
Kaushal Mahida <99007202+userkaushal@users.noreply.github.com>
2024-06-10 06:46:16 +0000

    operators

---
+++ 02_basics/operators.js
@@ -0,0 +1,46 @@
+//operators in js
+console.log("operators in js");
+
+let a = 15;
+let b = 2;
+
+//Arithmetic operators
+
+console.log("a + b = ",a+b);
+console.log("a - b = ",a-b);
+console.log("a * b = ",a*b);
+console.log("a / b = ",a/b);
+console.log("a % b = ",a%b);
+console.log("a ** b = ",a**b);
+console.log("a++",a++);
+console.log("a--",a--);
+console.log("a = ",a);
+
+//Assignment operators
+
+let c=1;
+c += 5;
+console.log("c = ",c);
+
+
+//comparison operators - true or false
+
+let comp1 = 6;
+// let comp2 = 5;
+// let comp2 = 6;
+let comp2 = "6"
+
+console.log("comp1 == comp2 is:",comp1==comp2);
+console.log("comp1 != comp2 is:",comp1!=comp2);
+console.log("comp1 === comp2 is:",comp1===comp2);
+console.log("comp1 !== comp2 is:",comp1!==comp2);
+
+//Logical operators -- &&, ||, !
+
+let x = 5;
+let y = 6;
+
+console.log(x<y && x==5);
+console.log(x>y || x==5);
+console.log(!false);
+
